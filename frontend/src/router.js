aadbcda6c948bcb2310850eebc15ca8b5aa2e23f
Linus Romland <48645689+linusromland@users.noreply.github.com>
2022-01-20 09:30:09 +0100

    Configured Vue.JS for `HTML5 History Mode`

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,15 +1,21 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
 	{
+		name: 'Home',
 		component: () => import('./views/Home.vue'),
 		path: '/'
 	}
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes
+});
+
+router.beforeEach((to, from, next) => {
+	document.title = `${String(to.name)} | ChatBubble`;
+	next();
 });
 
 export default router;
