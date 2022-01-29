1b460bb998bd17697bab9001e79c0f4d30a7e6d8
Linus Romland <48645689+linusromland@users.noreply.github.com>
2022-01-29 10:17:47 +0100

    Changed home page design

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
 	<main
-		class="h-full flex items-center px-6 lg:px-32 bg-slate-900 text-white"
+		class="h-screen w-screen flex items-center px-6 lg:px-32 bg-slate-900 text-white"
 	>
-		<section class="w-full md:w-9/12 xl:w-8/12">
-			<h1 class="text-5xl lg:text-5xl font-bold text-sky-500">
-				ChatBubble
-			</h1>
-			<p class="font-bold text-2xl">
+		<section
+			class="w-full md:w-9/12 xl:w-8/12 flex flex-col items-center justify-center lg:block"
+		>
+			<h1 class="text-5xl font-bold text-sky-500">ChatBubble</h1>
+			<p class="font-bold text-2xl text-center lg:text-left">
 				The free and open source chat platform!
 			</p>
 			<router-link
@@ -28,5 +28,6 @@ export default {
 main {
 	background-image: url('../assets/wallpaper.png');
 	background-size: cover;
+	background-position: right;
 }
 </style>
