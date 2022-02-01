a854f5cbf704db9c3ab9150bdcb11657020c5c8c
Linus Romland <48645689+linusromland@users.noreply.github.com>
2022-02-01 11:27:56 +0100

    Changed Vite Cache Request

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -46,7 +46,7 @@ export default defineConfig({
 					{
 						handler: 'NetworkFirst',
 						// Cache GET requests to /api/rooms/
-						urlPattern: 'http://localhost:3000/api/rooms'
+						urlPattern: 'https://chatbubble.romland.dev/api/rooms'
 					}
 				]
 			}
